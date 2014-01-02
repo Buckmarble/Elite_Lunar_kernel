42c1d393c0ff9959a87a3f46e1336bde46ad5c3d
buckmarble <forrestmarble@gmail.com>
2014-01-02 18:46:38 -0500

    update defconfigs with lcd tweaks
    Conflicts:
    
    	arch/arm/configs/m7_stkclk_defconfig

--- build_m7_stkclk_kernel.sh
+++ build_m7_stkclk_kernel.sh
@@ -27,7 +27,7 @@
   m=$k/out/$c/system/lib/modules
   z=$c-$today
 
-TOOLCHAIN=/home/forrest/kernel/arm-eabi-4.7/bin/arm-eabi-
+TOOLCHAIN=/home/forrest/kernel/arm-eabi-4.7_2/bin/arm-eabi-
 export ARCH=arm
 export SUBARCH=arm
 
