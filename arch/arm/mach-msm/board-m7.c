ff0722974e81149cdbc4213437516a47cbc3fd2b
Bryan Maltais <brymaster5000@gmail.com>
2014-02-08 19:42:10 -0500

    msm-thermal control from crpalmer
    Conflicts:
    
    	drivers/thermal/msm_thermal.c

--- arch/arm/mach-msm/board-m7.c
+++ arch/arm/mach-msm/board-m7.c
@@ -4037,14 +4037,14 @@ static struct platform_device msm_tsens_device = {
 
 static struct msm_thermal_data msm_thermal_pdata = {
 	.sensor_id = 0,
-	.poll_ms = 1000,
+/*	.poll_ms = 1000,
 #ifdef CONFIG_CPU_OVERCLOCK
 	.limit_temp = 75,
 #else
 	.limit_temp = 51,
 #endif
 	.temp_hysteresis = 10,
-	.limit_freq = 918000,
+	.limit_freq = 918000,*/
 };
 
 static int __init check_dq_setup(char *str)
