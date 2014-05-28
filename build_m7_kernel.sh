6e1ce614ecfc83c1f903734d71c2eb8b7259adb5
buckmarble <forrestmarble@gmail.com>
2014-05-27 20:55:14 -0400

    script update

--- build_m7_kernel.sh
+++ build_m7_kernel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Path to build your kernel
-  k=~/kernel/elite_lunar_aosp
+  k=~/kernel/Elite_Lunar_kernel
 # Directory for the any kernel updater
   t=$k/packages
 # Date to add to zip
